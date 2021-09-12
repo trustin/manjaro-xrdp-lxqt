4c49a9dca0847ec0fbcfa99857a5d3a6378e24c4
Trustin Lee <t@motd.kr>
2021-09-12 19:12:52 +0900

    Clean up run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-docker stop manjaro-xrdp-lxqt || true
-docker rm manjaro-xrdp-lxqt || true
+docker stop manjaro-xrdp-lxqt >/dev/null 2>&1 || true
+docker rm manjaro-xrdp-lxqt >/dev/null 2>&1 || true
 docker create --name manjaro-xrdp-lxqt \
   --privileged \
   --env "PUID=$(id -u)" \
   --env "PUSER=$(id -un)" \
-  --publish 3389:3389 \
+  --publish 23389:3389 \
   --publish 2222:22 \
   --shm-size 1G \
   ghcr.io/trustin/manjaro-xrdp-lxqt:latest
